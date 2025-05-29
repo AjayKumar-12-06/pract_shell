2610e7205a4b5820dd9c2408a5839d545566469e
awsajju <aws.devops.123aws@gmail.com>
2025-05-29 06:20:04 +0530

    using varibales

---
+++ variables/var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+f_Name=Ajay
+l_Name=Kumar
+
+full_Name= $f_Name +" "+$l_Name
+
---
+++ variables/var1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hi Ajay how are u"
+echo "Hi Nannu i'm good and you"
+echo "Thank you for asking i'm also good Nannu"
