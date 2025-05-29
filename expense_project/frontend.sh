a42e3c4c5c7a19a881a17b4be238e1f9f84ed73d
awsajju <aws.devops.123aws@gmail.com>
2025-05-29 13:51:41 +0530

    created frontend server

---
+++ expense_project/frontend.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# check the root access or not
+USER_ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+log_file="/var/log/expense.log"
+log_folder=$(echo $0 | cut -d "." -f1)
+Timestamp=$(date +%y-%m-%d-%H-%M-%S)
+log_file_name=$log_file/$log_folder-$Timestamp
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2.... $R failure $N"
+        exit 1
+    else
+        echo -e "$2....$G Success $N"
+    fi
+}
+
+echo "Script started executing at: $Timestamp" &>>$log_file_name
+
+check_root() {
+    if [ $USER_ID -ne 0 ];then
+        echo -e " $R ERROR : You must have the sudo access to execute this $N"
+        exit 1
+    fi
+}
+
+
+
+check_root
+
+dnf install nginx -y
+validate $? "installing nginx"
+
+systemctl enable nginx
+validate $? "enables  nginx"
+
+systemctl start nginx
+validate $? "started nginx"
+
+rm -rf /usr/share/nginx/html/*
+validate $? "remove the existing code"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+validate $? "download the code"
+
+cd /usr/share/nginx/html
+validate $? "moving to html"
+
+unzip /tmp/frontend.zip
+validate $? "Unzip the code"
+
+cp /home/ec2-user/pract_shell/expense_project/frontend.conf /etc/nginx/default.d/frontend.conf
+
+systemctl restart nginx
+validate $? "restarted nginx"
