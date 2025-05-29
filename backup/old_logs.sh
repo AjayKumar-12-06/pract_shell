f13e940daad204e39c3e2d7929fc2a764c6c7e78
awsajju <aws.devops.123aws@gmail.com>
2025-05-29 16:45:42 +0530

    while loop

--- backup/old_logs.sh
+++ backup/old_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source_dir="/home/ec2-user/app.log"
+source_dir=/home/ec2-user/app.log
 
 File_Source=$(find . $source_dir -name ".log" -mtime +14)
 echo "Files to be deleted: $File_Source"
