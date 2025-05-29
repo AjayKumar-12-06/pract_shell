4645fafa532451ab56bb6b8c2a9a8b38c50bec3b
awsajju <aws.devops.123aws@gmail.com>
2025-05-29 16:42:49 +0530

    while loop

--- backup/old_logs.sh
+++ backup/old_logs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-while read -r line
+source_dir="/home/ec2-user/app.log"
+
+File_Source=$(find . $source_dir -name ".log" -mtime +14)
+echo "Files to be deleted: $File_Source"
+
+while read -r file
 do
-    echo $line
-done <<< ./colors/colors.sh
+    echo "Delete file: $file"
+    rm -rf $file
+
+done <<< $File_SOurce
