51f5757beed78502c5f71e55816f8a20e2eaeec3
awsajju <aws.devops.123aws@gmail.com>
2025-05-30 06:27:45 +0530

    checking nginx with port

--- interview_shell/nginx_port.sh
+++ interview_shell/nginx_port.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR : User must have the sudo access to execute this"
+    exit 1
+fi
+
 port=$(netstat -lntp | grep -i tcp | grep -i -w nginx | awk -F " " '{print $4}' | cut -d ":" -f2)
 
 if [ $port -eq 80 ]; then
