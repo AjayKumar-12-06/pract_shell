ab9a0e18017e3ce2dda67f07dc2ccb3132d46695
awsajju <aws.devops.123aws@gmail.com>
2025-05-30 06:06:08 +0530

    checking nginx

---
+++ interview_shell/nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR : User must have the sudo access to execute this"
+    exit 1
+fi
+
+status=systemctl status nginx
+
+if [ $status -ne 0 ];then
+    echo "Nginx is not runnig"
+    exit 1
+else    
+    echo "Nginx is running"
+    systemctl start nginx
+fi
+
